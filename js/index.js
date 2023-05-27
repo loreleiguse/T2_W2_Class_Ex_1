dd84f7b4294df1a9c3f91df339946a0d6eb6d45c
loreleiguse <231091@virtualwindow.co.za>
2023-05-27 16:00:27 +0200

    Complete Class Exercise

---
+++ js/index.js
@@ -0,0 +1,13 @@
+let tim = 12;
+let jane = 22;
+let john = 14;
+
+tim = tim + 10;
+
+let teamTotal = tim + jane;
+let victory = john / 2;
+let total = teamTotal + victory;
+
+let answer = "Tim & Jane now have " + total + " marbles";
+
+console.log(answer)
